d77feb6a735cc3d087414158b7ab01f60f682ab5
Saumyajit <saumyajit@users.noreply.github.com>
2025-11-18 18:12:39 +0530

    Refactor init method and add user type checks
    Refactor init method to include user type checks and menu initialization.

--- Module.php
+++ Module.php
@@ -1,20 +1,57 @@
 <?php
+/**
+  *
+  *
+  * @version 1.0
+  * @author Saumyajt Pramanik <saumyajitp8@gmail.com>
+  * @license GNU Lesser General Public License v3.0
+  *
+  * You can redistribute this library and/or modify it under the terms of
+  * the GNU LGPL as published by the Free Software Foundation,
+  * either version 3 of the License, or any later version.
+  * However you must not change author and copyright information.
+  */
+
+declare(strict_types = 1);
+
 namespace Modules\maintenance_calendar;
 
-use CModule;
-use CMenuItem;
+use APP;
+use CController as CAction;
+use CWebUser;
+use Zabbix\Core\CModule;
 
+/**
+ * Please see Core\CModule class for additional reference.
+ */
 class Module extends CModule {
-    public function init() {
-        $menu = \App::getMenu()->findOrAdd('main')->getSubmenu();
-        $reportsMenu = $menu->findByName('reports');
-        if ($reportsMenu) {
-            $reportsMenu->insertAfter(
-                new CMenuItem('maintenance_calendar', _('Maintenance Calendar'))
-                    ->setAction('maintenance.calendar')
-                    ->setIcon('fas fa-calendar-alt'),
-                'dashboard'
-            );
+
+        /**
+         * Initialize module.
+         */
+        public function init(): void {
+                // guests and non-admins don't need this host group editor
+                if (CWebUser::isGuest() || CWebUser::getType() == USER_TYPE_ZABBIX_USER) {
+                        return;
+                }
+                // Initialize main menu (CMenu class instance).
+                APP::Component()->get('menu.main')->findOrAdd(_('Reports'))->getSubmenu()->add((new \CMenuItem(_('Maintenance Calendar')))->setAction('maintenance.calendar'));
         }
-    }
+
+        /**
+         * Event handler, triggered before executing the action.
+         *
+         * @param CAction $action  Action instance responsible for current request.
+         */
+        public function onBeforeAction(CAction $action): void {
+        }
+
+        /**
+         * Event handler, triggered on application exit.
+         *
+         * @param CAction $action  Action instance responsible for current request.
+         */
+        public function onTerminate(CAction $action): void {
+        }
+
 }
