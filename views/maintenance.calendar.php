154488eb2f782fb2fc0d478dc101062b77429cfc
Saumyajit <saumyajit@users.noreply.github.com>
2025-11-18 19:13:44 +0530

    Update maintenance.calendar.php

--- views/maintenance.calendar.php
+++ views/maintenance.calendar.php
@@ -4,95 +4,69 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Maintenance Calendar</title>
-    <!-- Inclua os estilos do FullCalendar v5 -->
+
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/main.min.css' rel='stylesheet' />
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/daygrid/main.min.css' rel='stylesheet' />
     <link href='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/timegrid/main.min.css' rel='stylesheet' />
+
     <style>
-        .fc-time {
-            display: none !important;
-        }
-        /* Estilos para ocupar toda a tela */
-        html, body {
-            margin: 0;
-            padding: 0;
-            height: 100%; /* Ajuste para 100% para ocupar toda a tela */
-        }
-        /* Estilos para o calendário */
+        .fc-time { display: none !important; }
+        html, body { margin: 0; padding: 0; height: 100%; }
+
         #calendar {
-            width: 80%; /* Redimensiona o calendário para 80% da largura da tela */
-            margin: 0 auto; /* Centraliza o calendário na tela */
-            height: calc(100% - 60px); /* Ajusta a altura do calendário */
+            width: 80%;
+            margin: 0 auto;
+            height: calc(100% - 60px);
         }
-        /* Estilos para a janela modal */
+
         .maintenance-details {
             position: fixed;
             top: 0;
-            right: -50%; /* Inicia fora da tela */
-            width: 50%; /* Largura da seção de detalhes */
+            right: -50%;
+            width: 50%;
             height: 100%;
-            background-color: rgba(0, 0, 0, 0.5); /* Fundo semi-transparente */
-            transition: right 0.3s ease; /* Transição suave ao abrir e fechar */
-            z-index: 999; /* Coloca a seção acima do calendário */
+            background-color: rgba(0, 0, 0, 0.5);
+            transition: right 0.3s ease;
+            z-index: 999;
         }
         .maintenance-details-content {
-            background-color: #fff; /* Fundo branco para o conteúdo */
+            background-color: #fff;
             height: 100%;
-            overflow-y: auto; /* Adiciona barra de rolagem vertical se necessário */
+            overflow-y: auto;
             padding: 20px;
         }
-        .maintenance-details-header {
-            font-weight: bold; /* Título em negrito */
-            font-size: 1.2em; /* Tamanho da fonte aumentado */
-            margin-bottom: 10px; /* Espaçamento inferior */
-        }
         .maintenance-title {
-            background-color: #333; /* Fundo escuro para o título */
-            color: #fff; /* Texto branco */
+            background-color: #333;
+            color: #fff;
             padding: 10px;
             font-weight: bold;
             font-size: 1.2em;
         }
         .maintenance-info {
-            background-color: #666; /* Fundo mais escuro para as informações */
-            color: #fff; /* Texto branco */
+            background-color: #666;
+            color: #fff;
             padding: 20px;
         }
         .close {
             color: #aaa;
             float: right;
             font-size: 28px;
-            font-weight: bold;
             cursor: pointer;
         }
+
         #calendarTitle {
             text-align: center;
-            background-color: #076699; /* Fundo escuro para o título */
-            color: #fff; /* Texto branco */
+            background-color: #076699;
+            color: white;
             padding: 10px;
             font-weight: bold;
-            font-size: 1.5em; /* Tamanho da fonte aumentado */
-        }
-        /* Estilos para o seletor de idioma */
-        .language-selector {
-            text-align: center;
-            margin: 10px 0;
-        }
-        .language-selector select {
-            padding: 5px;
-            font-size: 1em;
+            font-size: 1.5em;
         }
     </style>
 </head>
 <body>
+
     <div id="calendarTitle">Maintenance Calendar</div>
-    <div class="language-selector">
-        <label for="language-select">Select Language:</label>
-        <select id="language-select">
-            <option value="en">English</option>
-            <option value="pt-br">Português</option>
-        </select>
-    </div>
     <div id="calendar"></div>
 
     <div class="maintenance-details">
@@ -110,7 +84,9 @@
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/moment/main.min.js'></script>
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@5.11.3/locales-all.min.js'></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js"></script>
+
     <script>
+
         function fetchMaintenanceData() {
             return new Promise(function(resolve, reject) {
                 var requestData = {
@@ -127,22 +103,18 @@
                     id: 1
                 };
 
-                // Fazer a chamada à API do Zabbix
                 $.ajax({
-                    url: "http://127.0.0.1:8080/api_jsonrpc.php",
+                    url: "https://127.0.0.1:8080/zabbix/api_jsonrpc.php",
                     method: "POST",
                     contentType: "application/json",
                     data: JSON.stringify(requestData),
                     success: function(response) {
-                        if (response.result) {
-                            resolve(response.result);
-                        } else {
-                            reject("Error fetching scheduled maintenance.");
-                        }
+                        if (response.result) resolve(response.result);
+                        else reject("Error fetching maintenance.");
                     },
                     error: function(error) {
-                        console.error("Error in Zabbix API call: " + JSON.stringify(error));
-                        reject("Error fetching scheduled maintenance.");
+                        console.error("API error: " + JSON.stringify(error));
+                        reject("Error fetching maintenance.");
                     }
                 });
             });
@@ -150,67 +122,58 @@
 
         function determineEventColor(startDate, endDate) {
             const now = moment();
+
             if (now.isBetween(startDate, endDate)) {
                 const timeToEnd = endDate.diff(now, 'minutes');
-                if (timeToEnd <= 20) {
-                    return '#FFA500'; // Laranja
-                } else {
-                    return '#008000'; // Verde
-                }
-            } else {
-                return '#FF0000'; // Vermelho
+                return timeToEnd <= 20 ? '#FFA500' : '#008000'; // Orange, Green
             }
+            return '#FF0000'; // Red
         }
 
         var calendar;
 
-        function initializeCalendar(maintenanceData, locale) {
+        function initializeCalendar(maintenanceData) {
             var events = [];
 
             maintenanceData.forEach(function(maintenance) {
                 maintenance.timeperiods.forEach(function(period) {
                     var startDate = moment.unix(period.start_date);
                     var endDate = moment.unix(period.start_date).add(period.period, 'seconds');
-                    var title = `${maintenance.name || 'No name'} (${startDate.format('L')} - ${endDate.format('L')})`;
-                    var coletandoDados = maintenance.maintenance_type === "0" ? 'Yes' : 'No';
-                    var description = maintenance.hosts.length > 0 ? maintenance.hosts.map(host => host.name).join(', ') : '';
-                    var backgroundColor = determineEventColor(startDate, endDate);
+
                     events.push({
-                        title: title,
+                        title: `${maintenance.name} (${startDate.format('L')} - ${endDate.format('L')})`,
                         start: startDate.toISOString(),
                         end: endDate.toISOString(),
-                        description: description,
+                        description: maintenance.hosts.length > 0 ?
+                                     maintenance.hosts.map(host => host.name).join(', ') : '',
                         maintenanceDescription: maintenance.description || '',
-                        coletandoDados: coletandoDados,
-                        backgroundColor: backgroundColor
+                        collectingData: maintenance.maintenance_type === "0" ? 'Yes' : 'No',
+                        backgroundColor: determineEventColor(startDate, endDate)
                     });
                 });
             });
 
-            var calendarEl = document.getElementById('calendar');
-            if (calendar) {
-                calendar.destroy();
-            }
-            calendar = new FullCalendar.Calendar(calendarEl, {
+            if (calendar) calendar.destroy();
+
+            calendar = new FullCalendar.Calendar(document.getElementById('calendar'), {
                 initialView: 'dayGridMonth',
-                locale: locale,
+                locale: 'en',   // 🔥 Hard-coded to English
                 headerToolbar: {
                     left: 'prev,next today',
                     center: 'title',
                     right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 },
                 events: events,
-                eventClick: function(info) {
-                    openDetails(info.event); 
-                },
+                eventClick: function(info) { openDetails(info.event); },
                 eventDidMount: function(info) {
                     if (info.el.querySelector('.fc-list-event-title')) {
-                        info.el.querySelector('.fc-list-event-title').style.backgroundColor = info.event.backgroundColor;
+                        info.el.querySelector('.fc-list-event-title').style.backgroundColor =
+                            info.event.backgroundColor;
                     }
                 },
-                dayMaxEvents: true, 
-                height: '100%', 
-                expandRows: true 
+                dayMaxEvents: true,
+                height: '100%',
+                expandRows: true
             });
 
             calendar.render();
@@ -219,44 +182,31 @@
         function openDetails(event) {
             var start = event.start ? moment(event.start).format('MMMM Do YYYY, h:mm:ss a') : '';
             var end = event.end ? moment(event.end).format('MMMM Do YYYY, h:mm:ss a') : '';
-            $('#infoContent').html(`
+
+            document.getElementById('infoContent').innerHTML = `
                 <div class="maintenance-info">
                     <p><strong>Title:</strong> ${event.title}</p>
                     <p><strong>Start:</strong> ${start}</p>
                     <p><strong>End:</strong> ${end}</p>
                     <p><strong>Hosts in Maintenance:</strong> ${event.extendedProps.description}</p>
                     <p><strong>Description:</strong> ${event.extendedProps.maintenanceDescription}</p>
-                    <p><strong>Data Collection:</strong> ${event.extendedProps.coletandoDados}</p>  
+                    <p><strong>Data Collection:</strong> ${event.extendedProps.collectingData}</p>  
                 </div>
-            `);
-            $('.maintenance-details').css('right', '0'); // Exibe a seção de detalhes movendo-a para a direita
+            `;
+
+            document.querySelector('.maintenance-details').style.right = '0';
         }
 
         function closeDetails() {
-            $('.maintenance-details').css('right', '-50%'); // Oculta a seção de detalhes movendo-a para fora da tela
+            document.querySelector('.maintenance-details').style.right = '-50%';
         }
 
         document.addEventListener('DOMContentLoaded', function() {
             fetchMaintenanceData()
-                .then(function(maintenanceData) {
-                    var locale = document.getElementById('language-select').value;
-                    initializeCalendar(maintenanceData, locale);
-                })
-                .catch(function(error) {
-                    console.error("Error fetching scheduled maintenance: " + error);
-                });
-
-            document.getElementById('language-select').addEventListener('change', function() {
-                fetchMaintenanceData()
-                    .then(function(maintenanceData) {
-                        var locale = document.getElementById('language-select').value;
-                        initializeCalendar(maintenanceData, locale);
-                    })
-                    .catch(function(error) {
-                        console.error("Error fetching scheduled maintenance: " + error);
-                    });
-            });
+                .then(initializeCalendar)
+                .catch(err => console.error(err));
         });
+
     </script>
 </body>
 </html>
