4733f310f06f499512902846eeba20a8c2b86108
Saumyajit <saumyajit@users.noreply.github.com>
2025-11-18 16:51:45 +0530

    Change namespace and update menu item label

--- Module.php
+++ Module.php
@@ -1,17 +1,16 @@
 <?php
-namespace Modules;
+namespace maintenance_calendar;
 
 use CModule;
 use CMenuItem;
 
 class Module extends CModule {
     public function init() {
-        // Register menu item under Reports
         $menu = \App::getMenu()->findOrAdd('main')->getSubmenu();
         $reportsMenu = $menu->findByName('reports');
         if ($reportsMenu) {
             $reportsMenu->insertAfter(
-                new CMenuItem('maintenance_calendar', 'Maintenance Calendar')
+                new CMenuItem('maintenance_calendar', _('Maintenance Calendar'))
                     ->setAction('maintenance.calendar')
                     ->setIcon('fas fa-calendar-alt'),
                 'dashboard'
